4eca167dad9fc9e4b97537c5d94371f168ea7305
Lehhernandes <lehernandesarruda@gmail.com>
2024-02-29 22:38:45 -0300

    aula 29/02/24 - endpoints iniciais

--- src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
+++ src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
@@ -2,12 +2,39 @@ package br.com.api.fatec.apifatec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.websocket.server.PathParam;
+
+@RestController
 @SpringBootApplication
 public class ApiFatecApplication {
-
+	@RequestMapping("/")
+	String home()
+	{
+		return "hello word";
+	}
+	@RequestMapping("/hello")
+	String home2()
+	{
+		return "hello word2";
+	}
+	
+	@RequestMapping("/numero")
+	int home3() {
+		return 2;
+	}
+	@RequestMapping("/numero/{num}")
+	int numero (@PathVariable int num) {
+		return num;
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFatecApplication.class, args);
 	}
 
 }
+ 
