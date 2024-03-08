f9dbf8cc8e3519a823773a4853573aa6ad4eaef4
Lehhernandes <lehernandesarruda@gmail.com>
2024-03-07 22:43:22 -0300

    som dos animais

--- src/main/java/br/com/api/fatec/apifatec/controllers/exercicios/AnimalController.java
+++ src/main/java/br/com/api/fatec/apifatec/controllers/exercicios/AnimalController.java
@@ -1,5 +1,15 @@
 package br.com.api.fatec.apifatec.controllers.exercicios;
 
+import java.util.List;
+
 public class AnimalController {
+	String[] types = {"cachorro", "gato", "vaca", "galinha"};	
+	public String[] getAnimalSounds() {
+		return types;
+	}
+	private String[] getSoundByAnimalType() {
+		return types;
+		
+	}
 
 }
