e10d0f43451647b4662b1d13720b019655ea1558
Lehhernandes <lehernandesarruda@gmail.com>
2024-03-08 13:29:37 -0300

    tudo errado

--- src/main/java/br/com/api/fatec/apifatec/controllers/exercicios/AnimalController.java
+++ src/main/java/br/com/api/fatec/apifatec/controllers/exercicios/AnimalController.java
@@ -2,10 +2,20 @@ package br.com.api.fatec.apifatec.controllers.exercicios;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class AnimalController {
-	String[] types = {"cachorro", "gato", "vaca", "galinha"};	
+	String[] types = {"cachorro", "gato", "vaca", "galinha"};
+	@RequestMapping("/animal/{nomes}")	
 	public String[] getAnimalSounds() {
+		for (int x=0; x<types.length;x++){
+			int x =Integer.parseInt(types);
+			if (types.get(x).getAnimalSounds.equalls{
+
+			}	
+		}
 		return types;
+
 	}
 	private String[] getSoundByAnimalType() {
 		return types;
