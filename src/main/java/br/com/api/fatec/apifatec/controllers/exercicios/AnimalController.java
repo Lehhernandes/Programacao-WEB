d810b38abbf3e136ab426118067b5b7cd4234e71
Lehhernandes <lehernandesarruda@gmail.com>
2024-03-13 19:25:12 -0300

    exercicio som dos animais

--- src/main/java/br/com/api/fatec/apifatec/controllers/exercicios/AnimalController.java
+++ src/main/java/br/com/api/fatec/apifatec/controllers/exercicios/AnimalController.java
@@ -1,25 +1,45 @@
 package br.com.api.fatec.apifatec.controllers.exercicios;
-
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AnimalController {
-	String[] types = {"cachorro", "gato", "vaca", "galinha"};
-	@RequestMapping("/animal/{nomes}")	
-	public String[] getAnimalSounds() {
-		for (int x=0; x<types.length;x++){
-			int x =Integer.parseInt(types);
-			if (types.get(x).getAnimalSounds.equalls{
 
-			}	
-		}
-		return types;
-
-	}
-	private String[] getSoundByAnimalType() {
-		return types;
+	//http://localhost:8090/api/exercicios/animals/cachorro,gato
+	@GetMapping("/animals/{types}")
+  public List<String> getAnimalSounds(@PathVariable String[] types) {
+     
 		
-	}
-
+        List<String> sounds = new ArrayList<>();
+        
+        //FORMA 1: USANDO FOR
+        for (String type : types) {
+            sounds.add(getSoundByAnimalType(type));
+        }
+        return sounds;
+        
+        //FORMA 2: USANDO MAP
+        /*return Arrays.stream(types)
+        .map(this::getSoundByAnimalType)
+        .collect(Collectors.toList());*/
+        
+  }
+	
+	private String getSoundByAnimalType(String type) {
+        switch (type.toLowerCase()) {
+            case "cachorro":
+                return "AuAu";
+            case "gato":
+                return "Miauu";
+            case "vaca":
+                return "Moo";
+            default:
+                return "animal desconhecido";
+        }
+  }
+	
 }
